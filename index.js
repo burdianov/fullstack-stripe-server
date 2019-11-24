c86cc58e07d009aa40c483692575cc38aaca690d
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-11-24 22:34:40 +0400

    add dev script

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({hi: 'there'});
+  res.send({it: 'has been changed'});
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}...`);
+});
