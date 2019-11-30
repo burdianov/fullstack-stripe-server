e3ccbae3816f4beb5345abe3ef005c7f20d820bf
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-11-30 23:03:09 +0400

    style the header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,16 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav>
+      <div className="nav-wrapper">
+        <a className="left brand-logo">Emaily</a>
+        <ul className="right">
+          <li>
+            <a>Login with Google</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 };
 
