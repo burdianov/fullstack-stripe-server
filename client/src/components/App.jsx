13c4ffa4e9a1fc4c2e90c72bfaa88ea580dfc26a
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-11-30 22:29:36 +0400

    setup routing

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import {BrowserRouter, Route} from "react-router-dom";
+import Header from "./Header";
+
+const Dashboard = () => {
+  return <h2>Dashboard</h2>
+};
+const SurveyNew = () => {
+  return <h2>SurveyNew</h2>
+};
+const Landing = () => {
+  return <h2>Landing</h2>
+};
 
 const App = () => {
   return <div>
-    <h1>App Component</h1>
+    <BrowserRouter>
+      <div>
+        <Header/>
+        <Route exact path="/" component={Landing}/>
+        <Route exact path="/surveys" component={Dashboard}/>
+        <Route path="/surveys/new" component={SurveyNew}/>
+      </div>
+    </BrowserRouter>
   </div>
 };
 
