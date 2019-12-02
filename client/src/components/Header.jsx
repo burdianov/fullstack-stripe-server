59424b48f1a3f9d24c2e77d1008df106dc71dc67
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-12-02 11:57:09 +0400

    access state in the header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,32 @@
 import React from "react";
+import {connect} from "react-redux";
 
-const Header = () => {
+const Header = ({auth}) => {
+  const renderContent = () => {
+    switch(auth) {
+      case null:
+        return "still deciding...";
+      case false:
+        return "I'm logged out";
+      default:
+        return "I'm logged in";
+    }
+  };
+
   return (
     <nav>
       <div className="nav-wrapper">
         <a className="left brand-logo">Emaily</a>
         <ul className="right">
-          <li>
-            <a>Login with Google</a>
-          </li>
+          {renderContent()}
         </ul>
       </div>
     </nav>
-  )
+  );
 };
 
-export default Header;
+const mapStateToProps = ({auth}) => {
+  return {auth}
+};
+
+export default connect(mapStateToProps)(Header);
