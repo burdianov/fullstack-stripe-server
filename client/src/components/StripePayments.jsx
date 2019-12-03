799d1f9c17680fdb4933d9f9f3a5fe8c0c9c4c64
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-12-03 23:05:10 +0400

    create StripePayments component

---
+++ client/src/components/StripePayments.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripePayments = () => {
+  debugger;
+  return (
+    <StripeCheckout
+      amount={500}
+      token={token => console.log(token)}
+      stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}
+    />
+  )
+};
+
+export default StripePayments;
