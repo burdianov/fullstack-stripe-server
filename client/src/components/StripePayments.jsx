9ea11abac59480056eddd969b205b2a71eb67e88
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-12-04 22:37:25 +0400

    posting the stripe token

--- client/src/components/StripePayments.jsx
+++ client/src/components/StripePayments.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import {connect} from "react-redux";
+import * as actions from "../actions";
 
-const StripePayments = () => {
+const StripePayments = ({handleToken}) => {
   return (
     <StripeCheckout
       name="Emaily"
       description="$5 for 5 email credits"
       amount={500}
-      token={token => console.log(token)}
+      token={token => handleToken(token)}
       stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}
     >
       <button className="btn">
@@ -17,4 +19,4 @@ const StripePayments = () => {
   )
 };
 
-export default StripePayments;
+export default connect(null, actions)(StripePayments);
