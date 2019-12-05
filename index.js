14bb0fe5400ce23053186172f1d9d8eaf6134531
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-12-05 23:04:01 +0400

    body-parser

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -13,6 +14,8 @@ mongoose.connect(keys.MONGO_URI,
   });
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({
