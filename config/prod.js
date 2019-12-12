7e292669d139d272800f2b82697232ed05608589
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-12-13 00:09:46 +0400

    setup sendgrid

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,6 @@ module.exports = {
   MONGO_URI: process.env.MONGO_URI,
   COOKIE_KEY: process.env.COOKIE_KEY,
   STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
-  STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY
+  STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+  SEND_GRID_KEY: process.env.SEND_GRID_KEY
 };
