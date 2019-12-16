b70cf5f2e6cbe67c8944a259b6fe7a61d1ae64f6
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-12-16 21:22:14 +0400

    show validation errors

--- client/src/components/surveys/SurveyField.jsx
+++ client/src/components/surveys/SurveyField.jsx
@@ -1,12 +1,13 @@
 // SurveyField contains logic to render a single label and a text input
 import React from "react";
 
-const SurveyField = ({input, label}) => {
+const SurveyField = ({input, label, meta: {error, touched}}) => {
   return (
-    <>
+    <div>
       <label>{label}</label>
       <input {...input}/>
-    </>
+      {touched && error}
+    </div>
   )
 };
 
