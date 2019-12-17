14f141c6028f3323dd00668626449f6afdb5c44b
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-12-17 22:47:18 +0400

    display invalid emails

--- client/src/components/surveys/SurveyForm.jsx
+++ client/src/components/surveys/SurveyForm.jsx
@@ -4,6 +4,7 @@ import _ from "lodash";
 import {Field, reduxForm} from "redux-form";
 import {Link} from "react-router-dom";
 import SurveyField from "./SurveyField";
+import validateEmails from "../../utils/validateEmails";
 
 const FIELDS = [
   {label: "Survey Title", name: "title"},
@@ -44,21 +45,13 @@ const SurveyForm = (props) => {
 const validate = (values) => {
   const errors = {};
 
+  errors.emails = validateEmails(values.emails || "");
+
   _.each(FIELDS, ({name}) => {
     if (!values[name]) {
       errors[name] = "You must provide a value";
     }
   });
-
-  if (!values.title) {
-    errors.title = "You must provide a title";
-  }
-  if (!values.subject) {
-    errors.subject = "You must provide a subject";
-  }
-  if (!values.body) {
-    errors.body = "You must provide a body";
-  }
 
   return errors;
 };
