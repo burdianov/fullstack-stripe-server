e4878f57718c13efff67f7910249fc99d8cb5696
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-12-18 22:29:26 +0400

    add local state to SurveyNew component

--- client/src/components/surveys/SurveyNew.jsx
+++ client/src/components/surveys/SurveyNew.jsx
@@ -1,11 +1,21 @@
 // SurveyNew shows SurveyForm and SurveyFormReview
-import React from "react";
+import React, {useState} from "react";
 import SurveyForm from "./SurveyForm";
+import SurveyFormReview from "./SurveyFormReview";
 
 const SurveyNew = () => {
+  const [showFormReview, setShowFormReview] = useState(false);
+
+  const renderContent = () => {
+    if (showFormReview) {
+      return <SurveyFormReview/>
+    }
+    return <SurveyForm onSurveySubmit={() => setShowFormReview(true)}/>
+  };
+
   return (
     <div>
-      <SurveyForm/>
+      {renderContent()}
     </div>
   )
 };
