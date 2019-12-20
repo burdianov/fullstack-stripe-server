6a3a8a8bf72ef61bc4311b2f5d69e1346ac76339
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-12-20 09:35:01 +0400

    outstanding form work

--- client/src/components/surveys/SurveyFormReview.jsx
+++ client/src/components/surveys/SurveyFormReview.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import {connect} from "react-redux";
 import formFields from "./formFields";
+import * as actions from "../../actions";
 
-const SurveyFormReview = ({onCancel, formValues}) => {
+const SurveyFormReview = ({onCancel, formValues, submitSurvey}) => {
   const reviewFields = formFields.map(({name, label}) => {
     return (
       <div key={name}>
@@ -17,10 +18,17 @@ const SurveyFormReview = ({onCancel, formValues}) => {
       <h5>Please confirm your entries</h5>
       {reviewFields}
       <button
-        className="yellow darken-3 btn-flat"
+        className="yellow darken-3 white-text btn-flat"
         onClick={onCancel}
       >
         Back
+      </button>
+      <button
+        onClick={() => submitSurvey(formValues)}
+        className="green btn-flat right white-text"
+      >
+        Send Survey
+        <i className="material-icons right">email</i>
       </button>
     </div>
   )
@@ -30,4 +38,4 @@ const mapStateToProps = (state) => {
   return {formValues: state.form.surveyForm.values}
 };
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(SurveyFormReview);
